24c969e5dc5f86dd905554a26804d7cb4347d680
Daidea <info.daidea@gmail.com>
2026-03-18 13:28:51 +0100

    Update vite.config.ts

--- scaffold/vite.config.ts
+++ scaffold/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // <--- ESTA LÍNEA ES VITAL
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
+      // Esto le dice a Vite que @ significa la carpeta src
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist', // Asegura que el output vaya a dist
+    emptyOutDir: true,
+  }
 });
