cb39ff90b48525e03b442ebe539c6c6f165b989b
Daidea <info.daidea@gmail.com>
2026-03-18 13:37:23 +0100

    Update vite.config.ts

--- scaffold/vite.config.ts
+++ scaffold/vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // <--- Esto es lo que resuelve el error
+import path from 'path' // <-- Esto es lo que resuelve el error
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Aquí le decimos a Vite que @ es la carpeta /src
+      // Aquí le decimos a Vite que @ significa la carpeta /src
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Asegura que los archivos vayan a la carpeta dist
     emptyOutDir: true,
   }
 })
